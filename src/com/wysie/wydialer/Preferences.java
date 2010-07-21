965fd0ad06533205be61bed1a0533197eabcad74
chertykov <chertykov@gmail.com>
2010-07-21 17:53:30 +0400

    Fix formatting to GNU coding standards.

--- src/com/wysie/wydialer/Preferences.java
+++ src/com/wysie/wydialer/Preferences.java
@@ -3,12 +3,14 @@ package com.wysie.wydialer;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
-public class Preferences extends PreferenceActivity {	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class Preferences extends PreferenceActivity
+{
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
         
-        // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.preferences);
-    }
+    // Load the preferences from an XML resource
+    addPreferencesFromResource(R.xml.preferences);
+  }
 }
